16e21439f123c7d5bb6223a02cd6f113156c682b
Neha Sinha <68558968+sinhaneha455@users.noreply.github.com>
2023-10-21 19:21:54 +0530

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Magic8BallTestGame
+{
+    partial class MagicEightBall
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MagicEightBall));
+            this.process1 = new System.Diagnostics.Process();
+            this.submitButton = new System.Windows.Forms.Button();
+            this.questionBox = new System.Windows.Forms.TextBox();
+            this.answerLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // process1
+            // 
+            this.process1.StartInfo.Domain = "";
+            this.process1.StartInfo.LoadUserProfile = false;
+            this.process1.StartInfo.Password = null;
+            this.process1.StartInfo.StandardErrorEncoding = null;
+            this.process1.StartInfo.StandardOutputEncoding = null;
+            this.process1.StartInfo.UserName = "";
+            this.process1.SynchronizingObject = this;
+            // 
+            // submitButton
+            // 
+            this.submitButton.BackColor = System.Drawing.Color.Black;
+            this.submitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.submitButton.ForeColor = System.Drawing.Color.White;
+            this.submitButton.Location = new System.Drawing.Point(12, 516);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(256, 40);
+            this.submitButton.TabIndex = 0;
+            this.submitButton.Text = "Tell me ! ";
+            this.submitButton.UseVisualStyleBackColor = false;
+            this.submitButton.Click += new System.EventHandler(this.submitButton_Click);
+            // 
+            // questionBox
+            // 
+            this.questionBox.Location = new System.Drawing.Point(12, 471);
+            this.questionBox.Multiline = true;
+            this.questionBox.Name = "questionBox";
+            this.questionBox.Size = new System.Drawing.Size(256, 39);
+            this.questionBox.TabIndex = 1;
+            this.questionBox.Text = "Ask your question here...";
+            // 
+            // answerLabel
+            // 
+            this.answerLabel.BackColor = System.Drawing.Color.Transparent;
+            this.answerLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.answerLabel.Location = new System.Drawing.Point(460, 208);
+            this.answerLabel.Name = "answerLabel";
+            this.answerLabel.Size = new System.Drawing.Size(179, 43);
+            this.answerLabel.TabIndex = 2;
+            this.answerLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // MagicEightBall
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1099, 568);
+            this.Controls.Add(this.answerLabel);
+            this.Controls.Add(this.questionBox);
+            this.Controls.Add(this.submitButton);
+            this.DoubleBuffered = true;
+            this.ForeColor = System.Drawing.Color.White;
+            this.Name = "MagicEightBall";
+            this.Text = "Magic 8 Ball";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Diagnostics.Process process1;
+        private System.Windows.Forms.TextBox questionBox;
+        private System.Windows.Forms.Button submitButton;
+        private System.Windows.Forms.Label answerLabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Magic8BallTestGame
+{
+    public partial class MagicEightBall : Form
+    {
+      //a random object which can help us to generate random indexes
+        Random random = new Random();
+
+        //stores the strings in answers list that means all possible answers
+        List<String> answers = new List<String>();
+
+        public MagicEightBall()
+        {
+            InitializeComponent();
+            // Yes answers
+            answers.Add("It is certain.");
+            answers.Add("It is decidedly so.");
+            answers.Add("Without a doubt.");
+            answers.Add("Yes definitely.");
+            answers.Add("You may rely on it.");
+            // Middle ground answers
+            answers.Add("As I see it, yes.");
+            answers.Add("Most likely.");
+            answers.Add("Outlook good.");
+            answers.Add("Yes.");
+            answers.Add("Signs point to yes.");
+            answers.Add("Reply hazy, try again.");
+            answers.Add("Ask again later.");
+            answers.Add("Better not tell you now.");
+            answers.Add("Cannot predict now.");
+            answers.Add("Concentrate and ask again.");
+            // No answers
+            answers.Add("Don't count on it.");
+            answers.Add("My reply is no.");
+            answers.Add("My sources say no.");
+            answers.Add("Outlook not so good.");
+            answers.Add("Very doubtful.");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void submitButton_Click(object sender, EventArgs e)
+        {
+            //get that random index
+            int index = random.Next(answers.Count);
+
+            // and then it will display that random index's text into answerLabel
+            answerLabel.Text = answers[index];
+
+        }
+    }
+}
